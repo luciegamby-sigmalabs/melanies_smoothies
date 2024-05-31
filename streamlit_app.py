f29ac118ca37fc66746b6d542da2caf01b5f415d
luciegamby-sigmalabs <156728701+luciegamby-sigmalabs@users.noreply.github.com>
2024-05-31 12:14:33 +0100

    Update streamlit_app.py
    Trialing var name change

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,9 @@ name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your smoothie will be: ", name_on_order)
 
 cnx = st.connection("snowflake")
-session = cnx.session()
+sesh = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = sesh.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 ingredients_list = st.multiselect("Choose up to 5 ingredients", my_dataframe, max_selections=5)
 
